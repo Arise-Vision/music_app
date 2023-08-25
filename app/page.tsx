e72dc8fffbe6ed204d915618098771bdbce8c40e
ArnaudBand <bandonkeyea@gmail.com>
2023-08-26 01:16:55 +0300

    Install button from shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <p className='text-6xl text-blue-400'>Hello BanZik</p>
+    <Button>Click here</Button>
   )
 }
